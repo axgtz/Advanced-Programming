a164644bb53ad546290d90a5aea598e63633a3be
axgtz <axguti@gmail.com>
2019-08-19 22:14:46 -0500

    started hwk matrix mult

---
+++ Hwk2-MatrixMultiplication/02-Roberto_Alejandro_Gutierrez.c
@@ -0,0 +1,11 @@
+//  A01019608
+// Created by Roberto Alejandro Gutierrez Guillen on 2019-08-19.
+//
+#include <unistd.h>
+
+int getopt (int argc, char *const *argv, const char *options);
+
+int main(){
+
+    return 0;
+}
