120541801ea2ea345f3932be8a27f25e420760a5
axgtz <axguti@gmail.com>
2019-09-19 22:08:15 -0500

    before traspass

--- Hwk4-VignereCypher/02-pipes.c
+++ Hwk4-VignereCypher/02-pipes.c
@@ -9,20 +9,20 @@
 #include <string.h>
 #include <unistd.h>     // UNIX functions
 
-#define BUFFER_LENGTH 100
+#define STR_SIZE 100
 
 void preparePipes(int * in_pipe, int * out_pipe);
 
-int main()
-{
+int main(){
     pid_t new_pid;
-    int status;
-    char buffer[BUFFER_LENGTH];
+    int status;                             // pipe status
+    int option = 0;
+    char fileName[STR_SIZE];                // Name of the file to read
+    char encryptionKey[STR_SIZE];          // Encryption key
+
     // Pipe arrays
     int parent_to_child[2];
     int child_to_parent[2];
-
-    printf("My PID is %d, and my parent is: %d\n", getpid(), getppid());
 
     // Open the pipes
     status = pipe(parent_to_child);
@@ -30,30 +30,98 @@ int main()
     status = pipe(child_to_parent);
     if (status < 0) {perror("PIPE"); exit(EXIT_FAILURE); }
 
-    printf("Parent to child: %d %d\n", parent_to_child[0], parent_to_child[1]);
-
     // Create a new process
     new_pid = fork();
+
     // Identify the processes
-    if (new_pid > 0)    // Parent process
-    {
+    if (new_pid > 0) {   // Parent process
         preparePipes(child_to_parent, parent_to_child);
-        printf("I am the parent (%d) and my child is (%d)\n", getpid(), new_pid);
-        sprintf(buffer, "Hello child\n"); // Overwrites buffer
-        write(parent_to_child[1], buffer, strlen(buffer)); // do a single write function
-    }
-    else if (new_pid == 0)
-    {
+
+        printf("VIGNERE CYPHER\n");
+
+        printf("Please write the name of the file.\n");
+        fgets(fileName, STR_SIZE, stdin);          // Get the name of the file
+        fileName[strlen(fileName)-1] = '\0';      // Remove the newline character
+        write(parent_to_child[1], fileName, strlen(fileName)); // do a single write function
+
+    }else if (new_pid == 0) {// Child process
         preparePipes(parent_to_child, child_to_parent);
-        printf("I am the child (%d), with parent (%d)\n", getpid(), getppid());
-        read(parent_to_child[0], buffer, BUFFER_LENGTH);
-        printf("My parent sent: %s", buffer);
-    }
-    else
-    {
+        // Read the name of the file
+        read(parent_to_child[0], fileName, STR_SIZE);
+        printf("My parent sent: %s", fileName);
+
+        // User choose to decode or encode a message
+        printf("Do you want to encode or decode a message? Write 1 for encode or 2 for decode\n");
+        scanf("%d", &option);
+        fgetc(stdin);   // Remove the enter left from scanf
+        if (option != 1 || option != 2){
+            printf("Please write a valid option next time\n");
+            exit(1);
+        }
+        /*
+        printf("Please write the encryption key MAX LENGTH = 99\n");
+        fgets(encryptionKey, STR_SIZE, stdin);
+        encryptionKey[strlen(encryptionKey)-1] = '\0';      // Remove the newline character
+        printf("%s", encryptionKey);
+v
+        */
+    /*
+        FILE *file_ptr_read = NULL;    // Pointer to file being read
+        FILE *file_ptr_write = NULL;   // Pointer to file being writen
+        char lineFileBuffer[STR_SIZE];        // Line from file
+        char *resultFileName = NULL;           // Used for the file name of the encoded file
+
+        strncpy(encryptionKey, "prograavanzada", STR_SIZE);
+
+        // Open file for reading
+        file_ptr_read = fopen(fileName, "r");
+
+        if (!file_ptr_read) {// Check that file is valid
+            perror("Error! reading the file");
+            // Program exits if the file pointer returns NULL.
+            exit(EXIT_FAILURE);
+        }
+
+        // Create new name for the result file
+
+        resultFileName = malloc(strlen(fileName) + 12 + 1); // 12 of the encoded and text, 1 for the end char
+        strcpy(resultFileName, "encoded_");         // Add encoded at the beginning
+        strcat(resultFileName, fileName);              // Add file name
+
+        // Open file for writing
+        file_ptr_write = fopen(resultFileName, "w");
+
+        if (!file_ptr_write) {// Check that file is valid
+            perror("Error! creating the file");
+            // Program exits if the file pointer returns NULL.
+            exit(EXIT_FAILURE);
+        }
+
+        size_t sizeEncKey = strlen(encryptionKey); // Size of the encryption key
+        // while it keeps reading
+        while (fgets(lineFileBuffer, STR_SIZE, file_ptr_read)) {
+            // Start reading
+            size_t sizeFile = strlen(lineFileBuffer); // Size of the stored data inside the buffer
+
+            // Encryption
+            for (int i = 0; i < sizeFile; i++) {
+                if (lineFileBuffer[i] > 96 && lineFileBuffer[i] < 123) { // if its within the alphabet
+                    int tot = ((lineFileBuffer[i] - 97) + (encryptionKey[i % sizeEncKey]) - 97) % 26;
+                    lineFileBuffer[i] = 97 + tot; // 97 is the ASCII number for 'a'
+                }
+            }
+
+            fprintf(file_ptr_write, "%s", lineFileBuffer); // Write all the lineFileBuffer into the file
+        }
+
+        fclose(file_ptr_read);
+        fclose(file_ptr_write);
+
+        free(resultFileName); // Free memory of string
+*/
+    }else{
         printf("ERROR when doing the fork\n");
     }
-    //printf("After fork my PID is %d, parent is: %d\n", getpid(), getppid());
 
     return 0;
 }
--- Hwk4-VignereCypher/cypherSolver.c
+++ Hwk4-VignereCypher/cypherSolver.c
@@ -8,70 +8,6 @@
 #define STR_SIZE 100
 
 int main () {
-    FILE * file_ptr_read = NULL;    // Pointer to file being read
-    FILE * file_ptr_write = NULL;   // Pointer to file being writen
-
-    int option = 0;
-    char fileName[STR_SIZE];                // Name of the file to read
-    char encryptionKey[STR_SIZE];          // Encryption key
-    char lineFileBuffer[STR_SIZE];        // Line from file
-
-    // Used to test, should be used as input
-    strncpy(fileName, "example_encodings/aladdin.txt", STR_SIZE);
-    /*
-     *  char *resultFileName = malloc(tamFile + 12 + 1);
-    strcpy(resultFileName, "encoded_");
-    strcat(resultFileName, fileName);
-    strcat(resultFileName, ".txt");
-     * */
-    strncpy(encryptionKey, "lemon", STR_SIZE);
-
-    file_ptr_read = fopen(fileName, "r");
-    printf("%s", fileName);
-    if (!file_ptr_read){// Check that file is valid
-        perror("Error! reading the file");
-        // Program exits if the file pointer returns NULL.
-        exit(EXIT_FAILURE);
-    }
-
-    size_t tamFile = strlen(lineFileBuffer); // Size of the stored data inside the buffer
-    size_t tamEncKey = strlen(encryptionKey); // Size of the encryption key
-    fgets(lineFileBuffer, STR_SIZE, file_ptr_read);
-
-    fclose(file_ptr_read);
-
-    // Encryption
-    for(int i = 0; i < tamFile;i++){
-        if(lineFileBuffer[i] > 96 && lineFileBuffer[i] < 123){ // if its within the alphabet
-            int tot = ((lineFileBuffer[i]-97) + (encryptionKey[i%tamEncKey])-97) %26;
-            lineFileBuffer[i] = 97 + tot; // 97 is the ASCII number for 'a'
-        } else {// Non-alphabet character, keeps it the same
-            lineFileBuffer[i] = lineFileBuffer[i];
-        }
-    }
-    for (int j = 0; j < tamFile; ++j) { // Print result encoded
-        printf("%c", lineFileBuffer[j]);
-    }
-    // Opening a file for writing
-    // Create new name for the result file
-    char *resultFileName = malloc(tamFile + 12 + 1);
-    strcpy(resultFileName, "encoded_");
-    strcat(resultFileName, fileName);
-    strcat(resultFileName, ".txt");
-
-    // Open file for writing
-    file_ptr_write = fopen(resultFileName, "w");
-
-    if (!file_ptr_write){// Check that file is valid
-        perror("Error! creating the file");
-        // Program exits if the file pointer returns NULL.
-        exit(EXIT_FAILURE);
-    }
-    fprintf(file_ptr_write, "%s",lineFileBuffer); // Write all the lineFileBuffer into the file
-    fclose(file_ptr_write);
-
-    free(resultFileName); // Free memory of string
-    /*
     // User choose to decode or encode a message
     printf("VIGNERE CYPHER\n");
     printf("Do you want to encode or decode a message? Write 1 for encode or 2 for decode\n");
@@ -84,9 +20,6 @@ int main () {
         fileName[strlen(fileName)-1] = '\0';      // Remove the newline character
         printf("%s", fileName);
 
-        // Get file and iterate through each char
-        // Ignore special characters and pass them directly. if they are not within a certain range
-        // Non special chars need to be evaluated with encryption key
 
     }else if(option == 2){ // Decode
         printf("Please write the encryption key MAX LENGTH = 99\n");
@@ -97,7 +30,65 @@ int main () {
 
     }else{
         printf("Please write a valid option next time\n");
-    }*/
+    }
+
+    FILE * file_ptr_read = NULL;    // Pointer to file being read
+    FILE * file_ptr_write = NULL;   // Pointer to file being writen
+
+    int option = 0;
+    char fileName[STR_SIZE];                // Name of the file to read
+    char encryptionKey[STR_SIZE];          // Encryption key
+    char lineFileBuffer[STR_SIZE];        // Line from file
+    char *resultFileName = NULL;           // Used for the file name of the encoded file
+
+    strncpy(fileName, "aladdin.txt", STR_SIZE);
+    strncpy(encryptionKey, "prograavanzada", STR_SIZE);
+
+    // Open file for reading
+    file_ptr_read = fopen(fileName , "r");
+
+    if (!file_ptr_read){// Check that file is valid
+        perror("Error! reading the file");
+        // Program exits if the file pointer returns NULL.
+        exit(EXIT_FAILURE);
+    }
+
+    // Create new name for the result file
+
+    resultFileName = malloc(strlen(fileName) + 12 + 1); // 12 of the encoded and text, 1 for the end char
+    strcpy(resultFileName, "encoded_");         // Add encoded at the beginning
+    strcat(resultFileName, fileName);              // Add file name
+
+    // Open file for writing
+    file_ptr_write = fopen(resultFileName, "w");
+
+    if (!file_ptr_write) {// Check that file is valid
+        perror("Error! creating the file");
+        // Program exits if the file pointer returns NULL.
+        exit(EXIT_FAILURE);
+    }
+
+    size_t sizeEncKey = strlen(encryptionKey); // Size of the encryption key
+    // while it keeps reading
+    while (fgets(lineFileBuffer, STR_SIZE, file_ptr_read)) {
+        // Start reading
+        size_t sizeFile = strlen(lineFileBuffer); // Size of the stored data inside the buffer
+
+        // Encryption
+        for (int i = 0; i < sizeFile; i++) {
+            if (lineFileBuffer[i] > 96 && lineFileBuffer[i] < 123) { // if its within the alphabet
+                int tot = ((lineFileBuffer[i] - 97) + (encryptionKey[i%sizeEncKey]) - 97) % 26;
+                lineFileBuffer[i] = 97 + tot; // 97 is the ASCII number for 'a'
+            }
+        }
+
+        fprintf(file_ptr_write, "%s", lineFileBuffer); // Write all the lineFileBuffer into the file
+    }
+
+    fclose(file_ptr_read);
+    fclose(file_ptr_write);
+
+    free(resultFileName); // Free memory of string
 
     return  0;
 }
@@ -112,10 +103,9 @@ You can use any of the Inter Process Communication (IPC) methods seen so far
  (pipe, popen, fifo, passing arguments), as long as you have two way communication
  between the parent and the child processes.
  */
-/* for (int j = 0; j < tamFile; ++j) { // Print result encoded
-     printf("%c", lineFile[j]);
- }*/
-
+/*for (int j = 0; j < tamFile; ++j) { // Print result encoded
+    printf("%c", lineFileBuffer[j]);
+}*/
 
 /* Print the ASCII characters
  * for (int i = 0; i < 26 ;i++){
